e617b2f909857749be9967b283615f21324a2325
Emma Van Oss <e.vanoss@gmail.com>
2019-12-19 17:30:13 +0000

    tests for Aged Brie and Sulfuras, Hand of Ragnaros

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -22,11 +22,45 @@ describe('Gilded Rose', () => {
     });
   });
 
-  describe('Overdue items', () => {
+  describe('Overdue regular items', () => {
     it('should reduce quality by 2', () => {
       const gildedRose = new Shop([new Item('foo', 0, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toEqual(8);
+    });
+  });
+
+  describe('Aged brie', () => {
+    it('should increase quality by 1', () => {
+      const gildedRose = new Shop([new Item('Aged Brie', 20, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(11);
+    });
+
+    it('should increase quality by 2 when overdue', () => {
+      const gildedRose = new Shop([new Item('Aged Brie', 0, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(12);
+    });
+
+    it('should not increase quality above 50', () => {
+      const gildedRose = new Shop([new Item('Aged Brie', 20, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(50);
+    });
+  });
+
+  describe('Sulfuras', () => {
+    it('should never change in quality', () => {
+      const gildedRose = new Shop([new Item('Sulfuras, Hand of Ragnaros', 5, 80)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(80);
+    });
+
+    it('should never change the sellIn value', () => {
+      const gildedRose = new Shop([new Item('Sulfuras, Hand of Ragnaros', 5, 80)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(5);
     });
   });
 });
