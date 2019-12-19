33286483f4d932df2174d69a5c775aba8f204351
Emma Van Oss <e.vanoss@gmail.com>
2019-12-19 17:18:32 +0000

    tests for regular items: reduces quality by 1, does not reduce below 0, lowers sellIn value

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -3,10 +3,22 @@ const { Item } = require('../src/item.js');
 
 describe('Gilded Rose', () => {
   describe('Regular items', () => {
-    it('should foo', () => {
-      const gildedRose = new Shop([new Item('foo', 0, 0)]);
+    it('should reduce quality by 1', () => {
+      const gildedRose = new Shop([new Item('foo', 20, 10)]);
       const items = gildedRose.updateQuality();
-      expect(items[0].name).toEqual('fixme');
+      expect(items[0].quality).toEqual(9);
+    });
+
+    it('should not reduce quality below 0', () => {
+      const gildedRose = new Shop([new Item('foo', 20, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(0);
+    });
+
+    it('should lower the SellIn value by 1', () => {
+      const gildedRose = new Shop([new Item('foo', 20, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(19);
     });
   });
 });
