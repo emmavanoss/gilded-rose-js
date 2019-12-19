fb31cf0d0b68107c59c284ef7ec20ac2fe42a1ed
Emma Van Oss <e.vanoss@gmail.com>
2019-12-19 17:20:52 +0000

    tests for overdue items: reduces quality by 2

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -15,10 +15,18 @@ describe('Gilded Rose', () => {
       expect(items[0].quality).toEqual(0);
     });
 
-    it('should lower the SellIn value by 1', () => {
+    it('should lower the sellIn value by 1', () => {
       const gildedRose = new Shop([new Item('foo', 20, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toEqual(19);
+    });
+  });
+
+  describe('Overdue items', () => {
+    it('should reduce quality by 2', () => {
+      const gildedRose = new Shop([new Item('foo', 0, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(8);
     });
   });
 });
