cdc43dd582aefe7a8b20ced5d0851298eb2dd478
Emma Van Oss <e.vanoss@gmail.com>
2019-12-19 17:35:08 +0000

    tests for backstage passes

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -63,4 +63,44 @@ describe('Gilded Rose', () => {
       expect(items[0].sellIn).toEqual(5);
     });
   });
+
+  describe('Backstage passes', () => {
+    it('should increase quality by 1 if sellIn is over 10', () => {
+      const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 20, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(11);
+    });
+
+    it('should increase quality by 2 if sellIn is 10', () => {
+      const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(12);
+    });
+
+    it('should increase quality by 2 if sellIn is 6', () => {
+      const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 6, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(12);
+    });
+
+    it('should increase quality by 3 if sellIn is 5', () => {
+      const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 3, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(13);
+    });
+
+    it('should increase quality by 3 if sellIn is 1', () => {
+      const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 1, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(13);
+    });
+
+    it('should set quality to 3 if sellIn is 0', () => {
+      const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(0);
+    });
+  });
 });
+
+
