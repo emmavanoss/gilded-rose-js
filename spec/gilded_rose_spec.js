caa642524bba55998af8a642ecf64564ba41c6f4
Emma Van Oss <e.vanoss@gmail.com>
2019-12-20 16:02:03 +0000

    unit test for Shop: updateQuality() is called on each item

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,4 +1,14 @@
 const { Shop } = require('../src/gilded_rose.js');
 
 describe('Gilded Rose', () => {
+  let foo = {
+    updateQuality: () => {},
+  };
+
+  it('calls updateQuality on each item', () => {
+    const gildedRose = new Shop([foo]);
+    spyOn(foo, 'updateQuality');
+    gildedRose.updateQuality();
+    expect(foo.updateQuality).toHaveBeenCalled();
+  });
 });
