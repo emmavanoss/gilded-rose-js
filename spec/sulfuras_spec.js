4cdedceef7c3550bfc8b9f88e4388df513d672a5
Emma Van Oss <e.vanoss@gmail.com>
2019-12-20 15:26:26 +0000

    small tweaks to sulfuras unit test

--- spec/sulfuras_spec.js
+++ spec/sulfuras_spec.js
@@ -1,9 +1,11 @@
 const { Sulfuras } = require('../src/sulfuras.js');
 
 describe('Sulfuras', () => {
+  const itemName = 'Sulfuras, Hand of Ragnaros';
+
   it('should never change in quality', () => {
-    const sulfurasItem = new Sulfuras('Sulfuras, Hand of Ragnaros', 0, 80);
-    sulfurasItem.updateQuality();
-    expect(sulfurasItem.quality).toEqual(80);
+    const sulfuras = new Sulfuras(itemName, 0, 80);
+    sulfuras.updateQuality();
+    expect(sulfuras.quality).toEqual(80);
   });
 });
